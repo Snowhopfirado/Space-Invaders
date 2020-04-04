5a00459229e4d2801ee23bd701e39723732028e8
Snowhopfirado <62614653+Snowhopfirado@users.noreply.github.com>
2020-04-04 00:59:31 -0400

    Add files via upload

--- space.js
+++ space.js
@@ -1,7 +1,7 @@
 let gameDisplay = (function() {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
-    canvas.width=window.innerWidth;
+    canvas.width=1200;
     canvas.height=window.innerHeight;
 
     let Rectangle = function(x,y,width,height,id) {
@@ -24,44 +24,92 @@ let gameDisplay = (function() {
 
         constructor: function(lines) {
             for (let i=0;i < lines;i++) {
-            for (let j=0; j < (window.innerWidth / 100); j++) {
-                let newRect = new Rectangle((j*100)+60,(i*50)+30,30,30,enemies.length)
-                this.draw(newRect);
-                enemies.push(newRect);
+                for (let j=1; j < 15; j++) {
+                    let newRect = new Rectangle((j*72)+50,(i*50)+30,20,20,enemies.length)
+                    this.draw(newRect);
+                    enemies.push(newRect);
             }
         };
         },
         
-        eID: enemies
+        eID: enemies,
+        num: 4
     };
 
 
 })();
+let yes = false;
 
 let gameLogic = (function() {
-
+    
     return {
-        movement: function(array,undrawfn,drawfn) {
-            array.forEach(function(obj) {
-                if (obj.id < (array.length)/4) {
-                    undrawfn(obj);
-                };
-                obj.y+=50;
-                drawfn(obj);
-            });
-        }, 
+        mLeft: true,
+        mRight: false,
+        mDown: false,
         
-    };
+        moveLeft: function(array,undrawfn,drawfn,left,right,down) {
+            array.forEach(function(obj) {
+                if (obj.x <= 35) {
+                    left = false;
+                    right = true;
+                }
+            })
+            array.forEach(function(obj) {
+                if (left === true) {
+                    undrawfn(obj);
+                    obj.x-=30;
+                    drawfn(obj);
+                } else if (left === false) {
+                    down = true;
+                };
+            });
+        },
+        
+
+        moveRight: function(array, undrawfn, drawfn, left,right,down) {
+            array.forEach(function(obj) {
+                if (obj.x >= 1140) {
+                    right = false;
+                } 
+            });
+            array.forEach(function(obj) {
+                if (right === true) {
+                    undrawfn(obj);
+                    obj.x += 30;
+                    drawfn(obj);
+                } else if (right === false) {
+                    down = true;
+                    left = true;
+                };
+            });
+        },
+        moveDown: function(array,undrawfn,drawfn,down) {
+            array.forEach(function(obj) {
+                if (obj.y >= 600) {
+                    down = false;
+                }
+            });
+            array.forEach(function(obj) {
+                if (down === true) {
+                    undrawfn(obj);
+                    obj.y+=45;
+                    drawfn(obj);
+            }
+            });
+            down = false;
+        },
+        
+    }     
 })();
 
 
 let masterController = (function(gDisplay,gLogic) {
     let start = function() {
-        gDisplay.constructor(4);
+        gDisplay.constructor(gDisplay.num);
         console.log(gDisplay.eID);
     };
     start();
-    setInterval(function() {gLogic.movement(gDisplay.eID,gDisplay.undraw,gDisplay.draw)},1000);
-    
-
-})(gameDisplay,gameLogic);
+    setInterval(function() {gLogic.moveLeft(gDisplay.eID,gDisplay.undraw,gDisplay.draw,gLogic.mLeft,gLogic.mRight,gLogic.mDown)},500);
+    setInterval(function() {gLogic.moveRight(gDisplay.eID,gDisplay.undraw,gDisplay.draw,gLogic.mLeft,gLogic.mRight,gLogic.mDown)},500);
+    setInterval(function() {gLogic.moveDown(gDisplay.eID,gDisplay.undraw,gDisplay.draw,gLogic.mDown)},500);
+})(gameDisplay,gameLogic); 
