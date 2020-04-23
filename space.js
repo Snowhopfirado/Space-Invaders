3d486c5d9c9b3165422c24b9104358cf509876c4
Snowhopfirado <62614653+Snowhopfirado@users.noreply.github.com>
2020-04-23 15:03:45 -0400

    Add files via upload

--- space.js
+++ space.js
@@ -1,183 +1,206 @@
-let gameDisplay = (function() {
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-    canvas.width=1200;
-    canvas.height=window.innerHeight;
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+canvas.width=1200;
+canvas.height=window.innerHeight;
 
-    let Rectangle = function(color,x,y,width,height,id) {
-        this.color = color;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.id = id;
-    };
-    let playerRect = {
-        color:"purple",
-        x:580,
-        y:670,
-        width:40,
-        height:40,
-        x_left: 0,
-        x_right: 0
-    };
-    enemies = [];
-    return {
-        pRect:playerRect,
-        draw: function(rect) {
-            ctx.fillStyle = rect.color;
-            ctx.fillRect(rect.x,rect.y,rect.width,rect.height);
-        },
-       
-        undraw: function(rect) {
-            ctx.clearRect(rect.x,rect.y,rect.width,rect.height);
-        },
+let enemyRect = function(color,x,y,width,height,movementType,id) {
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.movementType = movementType;
+    this.id = id;
+};
+let Projectile = function(color,x,y,width,height,hitDetect) {
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.hitDetect = hitDetect;
+};
+let player = {
+    color: "green",
+    x: 580,
+    y: (window.innerHeight - 40),
+    width: 40,
+    height:40
+};
+let booleans = {
+    fire: false,
+    gameEnd: false,
+    playerLeft: false,
+    playerRight: false,
+    p:0,
+    pTimer:0
+};
 
-        constructor: function(lines,pdraw,player) {
-            for (let i=0;i < lines;i++) {
-                for (let j=1; j < 15; j++) {
-                    let newRect = new Rectangle("blue",(j*72)+50,(i*50)+30,20,20,enemies.length)
-                    this.draw(newRect);
-                    enemies.push(newRect);
-            }
-        let ground = new Rectangle("green",0,710,1200,window.innerHeight);
-        this.draw(ground);
-        pdraw(player);
-        };
-        },
-        
-        eID: enemies,
-        num: 4,
-    };
+let enemies = [];
+const enemyCopy = enemies.slice();
+let projectiles = [];
+let drawRect = function(fn) {
+ctx.fillStyle = fn.color;
+ctx.fillRect(fn.x,fn.y,fn.width,fn.height);
+};
+let undrawRect = function(fn) {
+ctx.clearRect(fn.x,fn.y,fn.width,fn.height);
+};
 
-})();
-
-let gameLogic = (function() {
-    return {
-        
-        checkMove: {
-            left: true,
-            right: false,
-            down: false,
-            temp: true
-        },
-        
-        moveLeft: function(array,undrawfn,drawfn,move) {
-            array.forEach(function(obj) {
-                if (obj.x <= 35) {
-                    move.down = true;
-                    move.left = false;
-                }
-            })
-            array.forEach(function(obj) {
-                if (move.left === true) {
-                    undrawfn(obj);
-                    obj.x-=30;
-                    drawfn(obj);
-                } else if (move.left === false) {
-                    move.right = true;
-                };
-            });
-        },
-        
-
-        moveRight: function(array, undrawfn, drawfn,move) {
-            array.forEach(function(obj) {
-                if (obj.x >= 1140) {
-                    move.down = true;
-                    move.right = false;
-                } 
-            });
-            array.forEach(function(obj) {
-                if (move.right === true) {
-                    undrawfn(obj);
-                    obj.x += 30;
-                    drawfn(obj);
-                } else if (move.right === false) {
-                    move.left = true;
-                };
-            });
-        },
-        moveDown: function(array,undrawfn,drawfn,move) {
-            array.forEach(function(obj) {
-                if (obj.y >= 690) {
-                    move.down = false;
-                }
-            });
-            array.forEach(function(obj) {
-                if (move.down === true) {
-                    undrawfn(obj);
-                    obj.y+=30;
-                    drawfn(obj);
-            }
-            });
-            move.down = false;
-            
-            
-        },
-        eMovement: function(fnLeft,fnRight,array,undrawfn,drawfn,move) {
-            if (move.left === true && move.temp === true) {
-                fnLeft(array,undrawfn,drawfn,move);
-            } else if (move.right === true && move.temp === true) {
-                fnRight(array,undrawfn,drawfn,move);
-            };
-                
-        },
-        eMovementDown: function(fnDown,array,undrawfn,drawfn,move) {
-            if (move.temp === true) {fnDown(array,undrawfn,drawfn,move);}
-        },
-        tempStop: function(move) {
-            switch(move.temp) {
-                case true:
-                move.temp = false;
-                break;
-
-                case false:
-                move.temp = true;
-                
-            };
-        },
-        playerMove: function(undrawfn,drawfn,player) {
-            if (player.x_left === 1 && player.x > 0) {
-            undrawfn(player);
-            player.x-=(player.x_left * 10);
-            drawfn(player);
-        } else if (player.x_right === 1 && player.x < 1200 && player.x < 1160) {
-            undrawfn(player)
-            player.x+=(player.x_right * 10);
-            drawfn(player);
-        }
+let start = function(lines) {
+    for (let i=0;i < lines;i++) {
+        for (let j=1; j < 15; j++) {
+            let newRect = new enemyRect("blue",(j*72)+50,(i*50)+30,20,20,0,enemies.length);
+            drawRect(newRect);
+            enemies.push(newRect);
     }
-    }     
-})();
-
-
-let masterController = (function(gDisplay,gLogic) {
-    let start = function() {
-        gDisplay.constructor(gDisplay.num,gDisplay.draw,gDisplay.pRect);
-        console.log(gDisplay.eID);
+drawRect(player);
+};
+};
+let playerMovement = function(p) {
+    if (booleans.playerLeft === true) {
+    undrawRect(player);
+    player.x-=10;
+    drawRect(player);
+    } else if (booleans.playerRight === true) {
+    undrawRect(player);
+    player.x+=10;
+    drawRect(player);
     };
-    start();
-    setInterval(function() {gLogic.eMovement(gLogic.moveLeft,gLogic.moveRight,gDisplay.eID,gDisplay.undraw,gDisplay.draw,gLogic.checkMove)},500);
-    setInterval(function() {gLogic.eMovementDown(gLogic.moveDown,gDisplay.eID,gDisplay.undraw,gDisplay.draw,gLogic.checkMove)},500);
-    window.addEventListener('click',function() {gLogic.tempStop(gLogic.checkMove)});
-    setInterval(function() {gLogic.playerMove(gDisplay.undraw,gDisplay.draw,gDisplay.pRect)},16.67);
-    window.onkeydown = function(event) {
-        if (event.keyCode === 37) {
-            gDisplay.pRect.x_left = 1;
-        } else if (event.keyCode === 39) {
-            gDisplay.pRect.x_right = 1;
+};
+
+let enemyMovement = {
+    moveLeft: function() {
+        enemies.forEach(function(obj) {
+            if (obj.x <= 35 && obj.movementType === 0) {
+                enemies.forEach(function(obj) {
+                    obj.movementType = 2;
+                });
+            };
+        });
+        enemies.forEach(function(obj) {
+            if (obj.movementType === 0) {
+                undrawRect(obj);
+                obj.x-=35;
+                drawRect(obj);
+            }
+        });   
+    },
+    moveRight: function() {
+        enemies.forEach(function(obj) {
+            if (obj.x >= 1140 && obj.movementType === 1) {
+                enemies.forEach(function(obj) {
+                    obj.movementType = 3;
+                });
+            };
+        });
+        enemies.forEach(function(obj) {
+            if (obj.movementType === 1) {
+                undrawRect(obj);
+                obj.x+=35;
+                drawRect(obj);
+            };
+        });
+    },
+    moveDown: function(rect) {
+        undrawRect(rect);
+        rect.y+=30;
+        drawRect(rect);
+    },
+    eMovement: function() {
+        if (booleans.p%24 === 0) {    
+            enemyMovement.moveLeft();
+            enemyMovement.moveRight();
         };
+    },
+    eMovementDown: function() {
+        if (booleans.p%24 === 0) {
+            enemies.forEach(function(obj) {
+                if (obj.movementType === 2) {
+                    enemyMovement.moveDown(obj);
+                    obj.movementType = 1;
+                };
+                if (obj.movementType === 3) {
+                    enemyMovement.moveDown(obj);
+                    obj.movementType = 0;
+                };
+            });
+        };
+    }
+};
+let fireProjectile = function() {
+    let newProjectile = new Projectile("yellow",(player.x + 12), (window.innerHeight - 60),11,15,false);
+    if (booleans.fire === true && booleans.pTimer%12 === 0 || booleans.pTimer === 1) {
+        drawRect(newProjectile);
+        projectiles.push(newProjectile);
     };
-    window.onkeyup = function(event) {
-        switch(event.keyCode) {
-            case 37:
-            gDisplay.pRect.x_left = 0;
-            break;
+        projectiles.forEach(function(obj) {
+           if (obj.hitDetect === false){
+            undrawRect(obj);
+            obj.y-=20;
+            drawRect(obj);
+           }
+        });
+    booleans.quickFire = false;
+};
+let hitDetection = function() {
+    enemies.forEach(function(e) {
+        projectiles.forEach(function(p) {
+            if (e.x < p.x + p.width &&
+                e.x + e.width > p.x &&
+                e.y < p.y + p.height &&
+                e.y + e.height > p.y) {
+                    p.hitDetect = true;
+                    undrawRect(e);
+                    undrawRect(p);
+                    indexE = enemies.indexOf(e);
+                    indexP = projectiles.indexOf(p);
+                    if (indexE > -1) {
+                        enemies.splice(indexE,1);
+                    };
+                    if (indexP > -1) {
+                        projectiles.splice(indexP,1);
+                    };
+                };
+        })
+    })
+};
 
-            case 39:
-            gDisplay.pRect.x_right = 0;
-            break;
-        }
+window.onkeydown = function(event) {
+    if (event.keyCode === 32) {
+        booleans.fire = true;
+        console.log(enemyCopy);
     };
-})(gameDisplay,gameLogic);
+    if (event.keyCode === 37 || event.keyCode === 65) {
+        booleans.playerLeft = true;
+    };
+    if (event.keyCode === 39 || event.keyCode === 68) {
+        booleans.playerRight = true;
+    };
+};
+window.onkeyup = function(event) {
+    if (event.keyCode === 32) {
+        booleans.fire = false;
+        booleans.pTimer = 0;
+    };
+    if (event.keyCode === 37 || event.keyCode === 65) {
+        booleans.playerLeft = false;
+    } else if (event.keyCode === 39 || event.keyCode === 68) {
+        booleans.playerRight = false;
+    }
+};
+
+let loop = function(p) {
+    booleans.p++;
+    if (booleans.fire === true) {
+        booleans.pTimer++
+    };
+    playerMovement();
+    hitDetection();
+    enemyMovement.eMovement();
+    enemyMovement.eMovementDown();
+    fireProjectile();
+    requestAnimationFrame(loop);
+};
+start(4);
+requestAnimationFrame(loop);
